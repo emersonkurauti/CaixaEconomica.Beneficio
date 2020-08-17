7be28db3a613174510b442bb0ee9e70fe2973451
emerson <emersonkurauti@gmail.com>
2020-08-17 20:24:29 -0300

    Adicionar arquivos de projeto.

---
+++ CaixaEconomica.Beneficio.Teste/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CaixaEconomica.Beneficio.Teste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
